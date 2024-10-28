a5cfbd26abccddf76eca1f9d9cb41d8b9c6f67e3
Kevin Cheng <kevincheng401@gmail.com>
2024-10-28 23:42:48 +0800

    Remove db container, configure connection to localdb

--- src/CoindeskApi/Program.cs
+++ src/CoindeskApi/Program.cs
@@ -22,7 +22,6 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen(options =>
 {
-    // using System.Reflection;
     var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 });
